0045a9e4cc6cfae459c27d150c49f23333284ceb
pedro-fe <pedrofe3107@gmai.com>
2024-03-19 21:35:30 -0300

    Projeto-versão1

---
+++ Projeto - The Duke/script.js
@@ -0,0 +1,21 @@
+  // Função JavaScript para rolar a página para baixo
+  function scrollDown() {
+    // Ajuste o valor do scroll para definir a posição para a qual deseja rolar
+    window.scrollBy({
+      top: window.innerHeight, // rolar uma tela para baixo
+      left: 0,
+      behavior: 'smooth' // animação suave
+    });
+  }
+
+  function scrollToTop() {
+    // Rola a página para o topo
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Adiciona uma animação suave de rolagem
+    });
+}
+
+
+
+
